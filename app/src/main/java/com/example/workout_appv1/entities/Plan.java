2eefd8e34340fb36b5bb20ed643ef479dd902cfd
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:09:32 +0100

    Transform Plan class to Entity class

--- app/src/main/java/com/example/workout_appv1/entities/Plan.java
+++ app/src/main/java/com/example/workout_appv1/entities/Plan.java
@@ -1,6 +1,11 @@
 package com.example.workout_appv1.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "plans")
 public class Plan {
+    @PrimaryKey(autoGenerate = true)
     private int planId;
     private String planName;
     private String goal;
