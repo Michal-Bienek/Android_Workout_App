f75c84474b44ce3b8baabbfc144152eb3b418c5d
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:16:14 +0100

    Transform ExercisesInRoutine to Entity class

--- app/src/main/java/com/example/workout_appv1/entities/ExercisesInRoutine.java
+++ app/src/main/java/com/example/workout_appv1/entities/ExercisesInRoutine.java
@@ -1,6 +1,21 @@
 package com.example.workout_appv1.entities;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "exercises_in_routine", foreignKeys = {
+        @ForeignKey(entity = Routine.class,
+        parentColumns = "routineId",
+        childColumns = "fk_routineId",
+        onDelete = ForeignKey.CASCADE),
+        @ForeignKey(entity = Exercise.class,
+        parentColumns = "exerciseId",
+        childColumns = "fk_exerciseId",
+        onDelete = ForeignKey.CASCADE)
+})
 public class ExercisesInRoutine {
+    @PrimaryKey(autoGenerate = true)
     private int exerciseInRoutineId;
     private int fk_routineId;
     private int fk_exerciseId;
