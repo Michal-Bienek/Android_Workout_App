c5152416521b05f9b62473ea87528ea45f323504
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:31:10 +0100

    Finished Plan class (entity)

--- app/src/main/java/com/example/workout_appv1/entities/Plan.java
+++ app/src/main/java/com/example/workout_appv1/entities/Plan.java
@@ -1,6 +1,7 @@
 package com.example.workout_appv1.entities;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "plans")
@@ -10,4 +11,44 @@ public class Plan {
     private String planName;
     private String goal;
     private boolean isActive;
+
+    @Ignore
+    public Plan(int planId, String planName, String goal, boolean isActive) {
+        this.planId = planId;
+        this.planName = planName;
+        this.goal = goal;
+        this.isActive = isActive;
+    }
+
+    public int getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(int planId) {
+        this.planId = planId;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
