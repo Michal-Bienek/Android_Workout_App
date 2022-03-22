9ffeb91e48b234240b59dd720274c8f571883452
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:11:13 +0100

    Transform Routine class to Entity class

--- app/src/main/java/com/example/workout_appv1/entities/Routine.java
+++ app/src/main/java/com/example/workout_appv1/entities/Routine.java
@@ -1,6 +1,16 @@
 package com.example.workout_appv1.entities;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "routines", foreignKeys = {@ForeignKey(entity =Plan.class,
+        parentColumns = "planId",
+        childColumns = "fk_planId",
+        onDelete = ForeignKey.CASCADE
+)} )
 public class Routine {
+    @PrimaryKey(autoGenerate = true)
     private int routineId;
     private String routineName;
     private int dayOfWeek;
