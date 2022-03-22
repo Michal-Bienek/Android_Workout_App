3cad6456b9cf988573a736f52cd94440bbf68925
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:33:31 +0100

    Finished Series class(Entity)

--- app/src/main/java/com/example/workout_appv1/entities/Series.java
+++ app/src/main/java/com/example/workout_appv1/entities/Series.java
@@ -1,6 +1,18 @@
 package com.example.workout_appv1.entities;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "series",foreignKeys = {
+        @ForeignKey(entity = ExercisesInRoutine.class,
+        parentColumns = "exerciseInRoutineId",
+        childColumns = "fk_exerciseInRoutineId",
+        onDelete = ForeignKey.CASCADE)
+})
 public class Series {
+    @PrimaryKey(autoGenerate = true)
     private int seriesId;
     private int reps;
     private double weight;
@@ -9,5 +21,70 @@ public class Series {
     private String note;
     private int fk_exerciseInRoutineId;
 
+    @Ignore
+    public Series(int seriesId, int reps, double weight, String rate, int restTime, String note, int fk_exerciseInRoutineId) {
+        this.seriesId = seriesId;
+        this.reps = reps;
+        this.weight = weight;
+        this.rate = rate;
+        this.restTime = restTime;
+        this.note = note;
+        this.fk_exerciseInRoutineId = fk_exerciseInRoutineId;
+    }
 
+    public int getSeriesId() {
+        return seriesId;
+    }
+
+    public void setSeriesId(int seriesId) {
+        this.seriesId = seriesId;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public int getRestTime() {
+        return restTime;
+    }
+
+    public void setRestTime(int restTime) {
+        this.restTime = restTime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getFk_exerciseInRoutineId() {
+        return fk_exerciseInRoutineId;
+    }
+
+    public void setFk_exerciseInRoutineId(int fk_exerciseInRoutineId) {
+        this.fk_exerciseInRoutineId = fk_exerciseInRoutineId;
+    }
 }
