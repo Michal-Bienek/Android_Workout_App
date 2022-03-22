d1ac744b4aeb3b8c8782b9397f98dc3de0a7feda
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:24:34 +0100

    Finished Exercise class (entity)

---
+++ app/src/main/java/com/example/workout_appv1/entities/Exercise.java
@@ -0,0 +1,53 @@
+package com.example.workout_appv1.entities;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+@Entity(tableName = "exercises")
+public class Exercise {
+    @PrimaryKey(autoGenerate = true)
+    private int exerciseId;
+    private String name;
+    private String description;
+    private String muscleGroup;
+
+    @Ignore
+    public Exercise(int exerciseId, String name, String description, String muscleGroup) {
+        this.exerciseId = exerciseId;
+        this.name = name;
+        this.description = description;
+        this.muscleGroup = muscleGroup;
+    }
+
+    public int getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(int exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+}
--- app/src/main/java/com/example/workout_appv1/entities/Exercises.java
+++
@@ -1,8 +0,0 @@
-package com.example.workout_appv1.entities;
-
-public class Exercises {
-    private int exerciseId;
-    private String name;
-    private String description;
-    private String muscleGroup;
-}
