67c656e8468b3922ac04d40f42f5490de22e0eb0
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-22 18:38:43 +0100

    Finished ExercisesInRoutine class (entity)

--- app/src/main/java/com/example/workout_appv1/entities/ExercisesInRoutine.java
+++ app/src/main/java/com/example/workout_appv1/entities/ExercisesInRoutine.java
@@ -2,6 +2,7 @@ package com.example.workout_appv1.entities;
 
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "exercises_in_routine", foreignKeys = {
@@ -20,4 +21,34 @@ public class ExercisesInRoutine {
     private int fk_routineId;
     private int fk_exerciseId;
 
+    @Ignore
+    public ExercisesInRoutine(int exerciseInRoutineId, int fk_routineId, int fk_exerciseId) {
+        this.exerciseInRoutineId = exerciseInRoutineId;
+        this.fk_routineId = fk_routineId;
+        this.fk_exerciseId = fk_exerciseId;
+    }
+
+    public int getExerciseInRoutineId() {
+        return exerciseInRoutineId;
+    }
+
+    public void setExerciseInRoutineId(int exerciseInRoutineId) {
+        this.exerciseInRoutineId = exerciseInRoutineId;
+    }
+
+    public int getFk_routineId() {
+        return fk_routineId;
+    }
+
+    public void setFk_routineId(int fk_routineId) {
+        this.fk_routineId = fk_routineId;
+    }
+
+    public int getFk_exerciseId() {
+        return fk_exerciseId;
+    }
+
+    public void setFk_exerciseId(int fk_exerciseId) {
+        this.fk_exerciseId = fk_exerciseId;
+    }
 }
