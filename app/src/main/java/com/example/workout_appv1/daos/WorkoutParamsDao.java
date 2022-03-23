71d3064c1850171d22a55b9e21678f2c3767bfba
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-23 11:12:42 +0100

    Created Base WorkoutParamsDao

--- app/src/main/java/com/example/workout_appv1/daos/WorkoutParamsDao.java
+++ app/src/main/java/com/example/workout_appv1/daos/WorkoutParamsDao.java
@@ -1,4 +1,43 @@
 package com.example.workout_appv1.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.workout_appv1.entities.WorkoutParams;
+
+import java.util.Date;
+import java.util.List;
+
+@Dao
 public interface WorkoutParamsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertWorkoutParams(WorkoutParams workoutParams);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertWorkoutParamsList(List<WorkoutParams> workoutParamsList);
+
+    @Delete
+    void deleteWorkoutParams(WorkoutParams workoutParams);
+
+    @Update
+    void updateWorkoutParams(WorkoutParams workoutParams);
+
+    @Query("UPDATE workout_params SET workout_date=:lastWorkoutDate WHERE workoutParamsId=:workoutParamsId")
+    void updateWorkoutParamsDate(int workoutParamsId, Date lastWorkoutDate);
+
+    @Query("SELECT * FROM workout_params")
+    List<WorkoutParams>getAllWorkoutParams();
+
+    @Query("SELECT * FROM workout_params WHERE fk_exerciseInRoutineId=:exerciseInRoutineId ORDER BY workoutParamsId ASC")
+    List<WorkoutParams>getWorkoutParamsInExerciseInRoutine(int exerciseInRoutineId);
+
+    @Query("SELECT * FROM workout_params WHERE fk_exerciseInRoutineId=:ExerciseInRoutineId ORDER BY workout_date DESC LIMIT 1")
+    List<WorkoutParams>getLatestWorkoutParamsInExerciseInRoutine(int ExerciseInRoutineId);
+
+
+
 }
