43957862f8a2f8d108235ae8e5aad7d5d52d1e39
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-23 10:31:57 +0100

    Created base ExerciseInRoutineDao

--- app/src/main/java/com/example/workout_appv1/daos/ExercisesInRoutineDao.java
+++ app/src/main/java/com/example/workout_appv1/daos/ExercisesInRoutineDao.java
@@ -1,4 +1,39 @@
 package com.example.workout_appv1.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.workout_appv1.entities.ExercisesInRoutine;
+
+import java.util.List;
+
+@Dao
 public interface ExercisesInRoutineDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertExerciseInRoutine(ExercisesInRoutine exerciseInRoutine);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertExerciseInRoutineList(List<ExercisesInRoutine> exercisesInRoutineList);
+
+    @Delete
+    void deleteExerciseInRoutine(ExercisesInRoutine exercisesInRoutine);
+
+    @Update
+    void updateExerciseInRoutine(ExercisesInRoutine exercisesInRoutine);
+
+    @Query("SELECT * FROM exercises_in_routine")
+    List<ExercisesInRoutine>getAllExercisesInRoutine();
+
+    @Query("SELECT * FROM exercises_in_routine WHERE fk_routineId=:routineId")
+    List<ExercisesInRoutine>getExercisesInRoutineByRoutineId(int routineId);
+
+    @Query("SELECT * FROM exercises_in_routine WHERE fk_exerciseId=:exerciseId")
+    List<ExercisesInRoutine>getExercisesInRoutineByExerciseId(int exerciseId);
+
+
+
 }
