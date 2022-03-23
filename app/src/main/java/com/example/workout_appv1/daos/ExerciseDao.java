878ef373c69bf60fb45c135dae6b2758e7f94a40
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-23 10:21:36 +0100

    Created base ExerciseDao

--- app/src/main/java/com/example/workout_appv1/daos/ExerciseDao.java
+++ app/src/main/java/com/example/workout_appv1/daos/ExerciseDao.java
@@ -1,4 +1,31 @@
 package com.example.workout_appv1.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Ignore;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.workout_appv1.entities.Exercise;
+
+import java.util.List;
+
+@Dao
 public interface ExerciseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertExercise(Exercise exercise);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertExercises(List<Exercise> exerciseList);
+
+    @Delete
+    void deleteExercise(Exercise exercise);
+
+    @Update
+    void updateExercise(Exercise exercise);
+
+    @Query("SELECT * FROM exercises")
+    List<Exercise>getAllExercises();
 }
