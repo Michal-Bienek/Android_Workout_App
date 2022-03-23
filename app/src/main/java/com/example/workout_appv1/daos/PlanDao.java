9e821418c6acdec8a8049aae66cf2b77a76e981a
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-03-23 09:56:36 +0100

    Created base Plan dao

--- app/src/main/java/com/example/workout_appv1/daos/PlanDao.java
+++ app/src/main/java/com/example/workout_appv1/daos/PlanDao.java
@@ -1,4 +1,36 @@
 package com.example.workout_appv1.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.workout_appv1.entities.Plan;
+
+import java.util.List;
+
+@Dao
 public interface PlanDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertPlan(Plan plan);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertPlans(List<Plan> plans);
+
+    @Delete
+    void deletePlan(Plan plan);
+
+    @Update
+    void updatePlan(Plan plan);
+
+    @Query("UPDATE plans SET isActive=:status WHERE planId==:id")
+    void updatePlanStatus(int id, boolean status);
+
+    @Query("SELECT * FROM plans ORDER BY isActive DESC")
+    List<Plan>getSortedPlans();
+
+
+
 }
