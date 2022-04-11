ef96f25a853450e30857609b636f78ee65550379
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-04-11 20:10:26 +0200

    Add navigation action from FragmentRoutine to FragmentExercise

--- app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentExercise.java
+++ app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentExercise.java
@@ -1,17 +1,22 @@
 package com.example.workout_appv1.ui.views.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.workout_appv1.R;
 
 public class FragmentExercise extends Fragment {
 
+    private int routineId;
+    private Context context;
 
     public FragmentExercise() {
     }
@@ -25,10 +30,22 @@ public class FragmentExercise extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context=context;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        FragmentExerciseArgs args=FragmentExerciseArgs.fromBundle(getArguments());
+        routineId=args.getRoutineId();
+        Toast.makeText(context, ""+routineId, Toast.LENGTH_SHORT).show();
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentRoutine.java
+++ app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentRoutine.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.workout_appv1.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FragmentRoutine extends Fragment {
 
@@ -20,6 +25,8 @@ public class FragmentRoutine extends Fragment {
     private int routineId;
     private Context context;
     private Button btnStartWorkout;
+    private RecyclerView rvRoutine;
+    private FloatingActionButton fabAddExerciseToPlan;
 
 
 
@@ -29,8 +36,7 @@ public class FragmentRoutine extends Fragment {
     }
 
     public static FragmentRoutine newInstance() {
-        FragmentRoutine fragment = new FragmentRoutine();
-        return fragment;
+        return new FragmentRoutine();
     }
 
     @Override
@@ -52,11 +58,26 @@ public class FragmentRoutine extends Fragment {
         FragmentRoutineArgs args= FragmentRoutineArgs.fromBundle(getArguments());
         routineId=args.getRoutineId();
 
+        initViews(view);
+
+        fabAddExerciseToPlan.setOnClickListener(view1 -> {
+            NavController navController= NavHostFragment.findNavController(this);
+            NavDirections action= FragmentRoutineDirections.actionFragmentRoutineToFragmentExercise(routineId);
+            navController.navigate(action);
+
+        });
+
+
+
+
+
         Toast.makeText(context, ""+routineId, Toast.LENGTH_SHORT).show();
         return view;
     }
 
-    private void initVariables(){
-
+    private void initViews(View view){
+        btnStartWorkout=view.findViewById(R.id.btnStartWorkout);
+        rvRoutine=view.findViewById(R.id.rvRoutine);
+        fabAddExerciseToPlan=view.findViewById(R.id.fabAddExerciseToPlan);
     }
 }
