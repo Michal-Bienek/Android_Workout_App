cb73acbdbedd8773a2ba0d2ea43a88af5aa1a9b8
Michal Bienek <michal.bienek@edu.uekat.pl>
2022-04-12 13:48:37 +0200

    Created DialogFragmentAddExerciseWithParams.java
    This dialog should handle adding exercise with parameters and series into List

---
+++ app/src/main/java/com/example/workout_appv1/ui/views/fragments/DialogFragmentAddExerciseWithParams.java
@@ -0,0 +1,37 @@
+package com.example.workout_appv1.ui.views.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.workout_appv1.R;
+
+public class DialogFragmentAddExerciseWithParams extends DialogFragment {
+    Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context=context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view= inflater.inflate(R.layout.fragment_dialog_add_exercise_with_params,container,false);
+
+
+       return view;
+    }
+}
