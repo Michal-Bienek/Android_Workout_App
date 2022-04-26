3b3a3ed596a9f1b5e216b067ef06612502d6ad6e
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-04-26 20:39:06 +0200

    Create RoutineRepository.java
    Created RoutineRepository.java which handles database operations

---
+++ app/src/main/java/com/example/workout_appv1/data/repositories/RoutineRepository.java
@@ -0,0 +1,51 @@
+package com.example.workout_appv1.data.repositories;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.workout_appv1.data.WorkoutPlannerDb;
+import com.example.workout_appv1.data.daos.RoutineDao;
+import com.example.workout_appv1.data.entities.Routine;
+import com.example.workout_appv1.data.relations.RoutineWithExercisesInRoutine;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RoutineRepository {
+    private final RoutineDao routineDao;
+    public RoutineRepository(Application application){
+        WorkoutPlannerDb database = WorkoutPlannerDb.getInstance(application);
+        this.routineDao = database.routineDao();
+    }
+
+    public void insertRoutine(Routine routine){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> routineDao.insertRoutine(routine));
+    }
+    public void insertRoutines(List<Routine>routines){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> routineDao.insertRoutines(routines));
+    }
+    public void updateRoutine(Routine routine){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(()->routineDao.updateRoutine(routine));
+    }
+    public void deleteRoutine(Routine routine){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(()->routineDao.deleteRoutine(routine));
+    }
+    public LiveData<List<Routine>>getAllRoutines(){
+        return routineDao.getAllRoutines();
+    }
+    public LiveData<List<Routine>>getRoutinesFromActivePlans(){
+        return routineDao.getRoutinesFromActivePlans();
+    }
+    public LiveData<List<Routine>>getAllPlanRoutines(int planId){
+        return routineDao.getAllPlanRoutines(planId);
+    }
+    public LiveData<List<RoutineWithExercisesInRoutine>>getRoutineWithExercisesInRoutineList(int routineId){
+        return routineDao.getRoutineWithExercisesInRoutineList(routineId);
+    }
+}
