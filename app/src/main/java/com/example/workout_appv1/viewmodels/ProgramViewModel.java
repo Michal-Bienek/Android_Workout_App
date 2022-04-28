359b6af7c0c186d2399914d8f35b7e40e4292ea4
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-04-28 10:58:40 +0200

    Create ProgramViewModel
    Created base ProgramViewModel.java

---
+++ app/src/main/java/com/example/workout_appv1/viewmodels/ProgramViewModel.java
@@ -0,0 +1,34 @@
+package com.example.workout_appv1.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.workout_appv1.data.WorkoutPlannerDb;
+import com.example.workout_appv1.data.entities.Plan;
+import com.example.workout_appv1.data.repositories.PlanRepository;
+
+import java.util.List;
+
+public class ProgramViewModel extends AndroidViewModel {
+    PlanRepository planRepository;
+
+    public ProgramViewModel(@NonNull Application application) {
+        super(application);
+        planRepository = new PlanRepository(application);
+    }
+    public void insertPlan(Plan plan){
+        planRepository.insertPlan(plan);
+    }
+    public void deletePlan(Plan plan){
+        planRepository.deletePlan(plan);
+    }
+    public void updatePlan(Plan plan){
+        planRepository.updatePlan(plan);
+    }
+    public LiveData<List<Plan>>getSortedPlans(){
+        return planRepository.getSortedPlans();
+    }
+}
