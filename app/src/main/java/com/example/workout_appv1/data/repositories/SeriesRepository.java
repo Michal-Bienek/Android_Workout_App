df800d40440ba6ad5e3befeac470861863ffde93
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-12 08:40:52 +0200

    Refactor SeriesRepository
    refactor SeriesRepository to use databases static ExecutorService instead making new every time

--- app/src/main/java/com/example/workout_appv1/data/repositories/SeriesRepository.java
+++ app/src/main/java/com/example/workout_appv1/data/repositories/SeriesRepository.java
@@ -13,27 +13,31 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SeriesRepository {
-    private SeriesDao seriesDao;
+    private final SeriesDao seriesDao;
     public SeriesRepository(Application application){
         WorkoutPlannerDb database = WorkoutPlannerDb.getInstance(application);
         seriesDao = database.seriesDao();
     }
 
     public void insertSeries(Series series){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->seriesDao.insertSeries(series));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->seriesDao.insertSeries(series));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->seriesDao.insertSeries(series));
     }
     public void insertSeriesList(List<Series> seriesList){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->seriesDao.insertSeriesList(seriesList));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->seriesDao.insertSeriesList(seriesList));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->seriesDao.insertSeriesList(seriesList));
     }
     public void deleteSeries(Series series){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->seriesDao.deleteSeries(series));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->seriesDao.deleteSeries(series));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->seriesDao.deleteSeries(series));
     }
     public void updateSeries(Series series){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->seriesDao.updateSeries(series));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->seriesDao.updateSeries(series));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->seriesDao.updateSeries(series));
     }
     public LiveData<List<Series>> getAllSeriesInWorkoutParams(int workoutParamsId){
         return seriesDao.getAllSeriesInWorkoutParams(workoutParamsId);
