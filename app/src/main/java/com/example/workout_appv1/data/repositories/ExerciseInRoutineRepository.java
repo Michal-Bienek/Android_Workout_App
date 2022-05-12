cf648636a435a7fbda61bec1150d97a046de07ae
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-12 08:34:23 +0200

    Refactor ExerciseInRoutineRepository
    refactor ExerciseInRoutineRepository to use databases static ExecutorService instead making new every time

--- app/src/main/java/com/example/workout_appv1/data/repositories/ExerciseInRoutineRepository.java
+++ app/src/main/java/com/example/workout_appv1/data/repositories/ExerciseInRoutineRepository.java
@@ -10,8 +10,10 @@ import com.example.workout_appv1.data.entities.ExercisesInRoutine;
 import com.example.workout_appv1.data.relations.ExercisesInRoutineWithWorkoutParams;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExerciseInRoutineRepository {
     private final ExercisesInRoutineDao exercisesInRoutineDao;
@@ -21,25 +23,36 @@ public class ExerciseInRoutineRepository {
     }
 
     public void insertExerciseInRoutine(ExercisesInRoutine exercisesInRoutine){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutine(exercisesInRoutine));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutine(exercisesInRoutine));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutine(exercisesInRoutine));
+//
+////        Future<Long>future = executorService.submit(()->this.exercisesInRoutineDao.insertExerciseInRoutine(exercisesInRoutine));
+//        Future<Long> submit = executorService.submit((Callable<Long>) () -> this.exercisesInRoutineDao.insertExerciseInRoutine(exercisesInRoutine));
+//        executorService.shutdown();
 
     }
 
     public void insertExerciseInRoutineList(List<ExercisesInRoutine> exercisesInRoutineList){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutineList(exercisesInRoutineList));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutineList(exercisesInRoutineList));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->this.exercisesInRoutineDao.insertExerciseInRoutineList(exercisesInRoutineList));
+//        executorService.shutdown();
     }
 
     public void deleteExerciseInRoutine(ExercisesInRoutine exercisesInRoutine){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(()->this.exercisesInRoutineDao.deleteExerciseInRoutine(exercisesInRoutine));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(()->this.exercisesInRoutineDao.deleteExerciseInRoutine(exercisesInRoutine));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(()->this.exercisesInRoutineDao.deleteExerciseInRoutine(exercisesInRoutine));
+//        executorService.shutdown();
 
     }
 
     public void updateExerciseInRoutine(ExercisesInRoutine exercisesInRoutine){
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(() -> exercisesInRoutineDao.updateExerciseInRoutine(exercisesInRoutine));
+        WorkoutPlannerDb.databaseWriteExecutor.execute(() -> exercisesInRoutineDao.updateExerciseInRoutine(exercisesInRoutine));
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        executorService.execute(() -> exercisesInRoutineDao.updateExerciseInRoutine(exercisesInRoutine));
+//        executorService.shutdown();
     }
 
     public LiveData<List<ExercisesInRoutine>> getAllExercisesInRoutine(){
