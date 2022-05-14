580945470bff10f655bb7ab714f5caf0583e79cd
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-14 13:54:09 +0200

    Update RoutineAdapter
    Updated RoutineAdapter to show exercises in routine

--- app/src/main/java/com/example/workout_appv1/ui/adapters/RoutineAdapter.java
+++ app/src/main/java/com/example/workout_appv1/ui/adapters/RoutineAdapter.java
@@ -1,18 +1,31 @@
 package com.example.workout_appv1.ui.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.workout_appv1.R;
+import com.example.workout_appv1.data.joinEntities.ExerciseInRoutineExercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoutineAdapter extends RecyclerView.Adapter<RoutineAdapter.RoutineViewHolder> {
+    private List<ExerciseInRoutineExercise> exerciseInRoutineExerciseList = new ArrayList<>();
+    private Context context;
+
+    public RoutineAdapter(Context context) {
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public RoutineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -22,12 +35,19 @@ public class RoutineAdapter extends RecyclerView.Adapter<RoutineAdapter.RoutineV
 
     @Override
     public void onBindViewHolder(@NonNull RoutineViewHolder holder, int position) {
+        ExerciseInRoutineExercise exercise = exerciseInRoutineExerciseList.get(position);
+        holder.tvExerciseNameItemRoutine.setText(exercise.exerciseName);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return exerciseInRoutineExerciseList.size();
+    }
+
+    public void setExerciseInRoutineExerciseList(List<ExerciseInRoutineExercise> exerciseInRoutineExerciseList) {
+        this.exerciseInRoutineExerciseList = exerciseInRoutineExerciseList;
+        notifyDataSetChanged();
     }
 
     public class RoutineViewHolder extends RecyclerView.ViewHolder{
