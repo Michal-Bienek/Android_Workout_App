757a3af8013264c9807c14e104c1abc7d21d1e9f
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-16 11:59:28 +0200

    Bind views FragmentWorkout

--- app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentWorkout.java
+++ app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentWorkout.java
@@ -1,5 +1,6 @@
 package com.example.workout_appv1.ui.views.fragments;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.workout_appv1.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentWorkout#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
 public class FragmentWorkout extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int routineId;
+    private TextView tvRestTimeFragmentWorkout, tvSeriesCountFragmentWorkout, tvExerciseNameFragmentWorkout;
+    private EditText etRepsFragmentWorkout, etWeightFragmentWorkout;
+    private FloatingActionButton btnNextFragmentWorkout;
+    private ImageView imgExerciseFragmentWorkout;
 
-    public FragmentWorkout() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentWorkout.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentWorkout newInstance(String param1, String param2) {
-        FragmentWorkout fragment = new FragmentWorkout();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout, container, false);
+        View view = inflater.inflate(R.layout.fragment_workout, container, false);
+        //Get argument
+        FragmentWorkoutArgs args = FragmentWorkoutArgs.fromBundle(getArguments());
+        routineId = args.getRoutineId();
+        //BindViews
+        bindViews(view);
+
+        return view;
+    }
+
+    private void bindViews(View view) {
+        this.tvRestTimeFragmentWorkout = view.findViewById(R.id.tvRestTimeFragmentWorkout);
+        this.tvSeriesCountFragmentWorkout = view.findViewById(R.id.tvSeriesCountFragmentWorkout);
+        this.tvExerciseNameFragmentWorkout = view.findViewById(R.id.tvExerciseNameFragmentWorkout);
+        this.etRepsFragmentWorkout = view.findViewById(R.id.etRepsFragmentWorkout);
+        this.etWeightFragmentWorkout = view.findViewById(R.id.etWeightFragmentWorkout);
+        this.btnNextFragmentWorkout = view.findViewById(R.id.btnNextFragmentWorkout);
+        this.imgExerciseFragmentWorkout = view.findViewById(R.id.imgExerciseFragmentWorkout);
+
     }
 }
