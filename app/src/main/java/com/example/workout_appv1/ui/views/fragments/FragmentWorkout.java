6e5ded5a7167c014bb433ea80da04673104b3428
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-18 12:02:29 +0200

    Add base action in FragmentWorkout

--- app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentWorkout.java
+++ app/src/main/java/com/example/workout_appv1/ui/views/fragments/FragmentWorkout.java
@@ -1,9 +1,12 @@
 package com.example.workout_appv1.ui.views.fragments;
 
+import android.content.Context;
 import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,20 +14,33 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.workout_appv1.R;
+import com.example.workout_appv1.data.WorkoutPlannerDb;
+import com.example.workout_appv1.data.daos.ExercisesInRoutineDao;
+import com.example.workout_appv1.data.entities.Series;
+import com.example.workout_appv1.data.joinEntities.ExerciseInRoutineWorkoutParams;
+import com.example.workout_appv1.data.joinEntities.ExerciseWithOneSeries;
+import com.example.workout_appv1.data.joinEntities.ExerciseWithSeries;
+import com.example.workout_appv1.viewmodels.FragmentWorkoutViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+import java.util.Map;
+
 public class FragmentWorkout extends Fragment {
 
-
+    Context context;
     private int routineId;
     private TextView tvRestTimeFragmentWorkout, tvSeriesCountFragmentWorkout, tvExerciseNameFragmentWorkout;
     private EditText etRepsFragmentWorkout, etWeightFragmentWorkout;
     private FloatingActionButton btnNextFragmentWorkout;
     private ImageView imgExerciseFragmentWorkout;
+    private FragmentWorkoutViewModel viewModel;
+    private ExerciseWithOneSeries exercise;
 
 
     @Override
@@ -33,17 +49,45 @@ public class FragmentWorkout extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_workout, container, false);
+        //Initialize viewmodel
+        viewModel = new ViewModelProvider(this).get(FragmentWorkoutViewModel.class);
         //Get argument
         FragmentWorkoutArgs args = FragmentWorkoutArgs.fromBundle(getArguments());
         routineId = args.getRoutineId();
         //BindViews
         bindViews(view);
 
+        exercise = viewModel.initializeVariables(routineId);
+        setViews(exercise);
+
+        btnNextFragmentWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExerciseWithOneSeries nextExercise = viewModel.getNextSeries(exercise);
+                if (nextExercise == null) {
+                    Toast.makeText(context, "ni ma wiyncyj ćwiczeń", Toast.LENGTH_SHORT).show();
+                } else {
+                    setExercise(nextExercise);
+                }
+            }
+        });
+
         return view;
+    }
+
+    private void setExercise(ExerciseWithOneSeries newExercise) {
+        this.exercise = newExercise;
+        setViews(exercise);
     }
 
     private void bindViews(View view) {
@@ -55,5 +99,15 @@ public class FragmentWorkout extends Fragment {
         this.btnNextFragmentWorkout = view.findViewById(R.id.btnNextFragmentWorkout);
         this.imgExerciseFragmentWorkout = view.findViewById(R.id.imgExerciseFragmentWorkout);
 
+    }
+
+    private void setViews(ExerciseWithOneSeries exercise) {
+        this.tvRestTimeFragmentWorkout.setText(String.valueOf(exercise.getSeries().getRestTime()));
+        int seriesPosition = exercise.getSeriesPosition() + 1;
+        String seriesCounter = seriesPosition + "/" + exercise.getSeries_Count();
+        this.tvSeriesCountFragmentWorkout.setText(seriesCounter);
+        this.tvExerciseNameFragmentWorkout.setText(exercise.getExercise().getName());
+        this.etRepsFragmentWorkout.setText(String.valueOf(exercise.getSeries().getReps()));
+        this.etWeightFragmentWorkout.setText(String.valueOf(exercise.getSeries().getWeight()));
     }
 }
