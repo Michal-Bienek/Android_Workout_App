6061ab1b854b2c1606b50a801694af1c76690ca1
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-18 11:52:54 +0200

    Add query getExercisesWithSeries to ExerciseInRoutineDao
    Add query which takes routineId and return Map which contains exerciseInRoutine as key and List of series where latest workout params date equals null

--- app/src/main/java/com/example/workout_appv1/data/daos/ExercisesInRoutineDao.java
+++ app/src/main/java/com/example/workout_appv1/data/daos/ExercisesInRoutineDao.java
@@ -4,16 +4,20 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.MapInfo;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.example.workout_appv1.data.entities.ExercisesInRoutine;
+import com.example.workout_appv1.data.entities.Series;
 import com.example.workout_appv1.data.joinEntities.ExerciseInRoutineExercise;
+import com.example.workout_appv1.data.joinEntities.ExerciseInRoutineWorkoutParams;
 import com.example.workout_appv1.data.relations.ExercisesInRoutineWithWorkoutParams;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Dao
@@ -50,6 +54,30 @@ public interface ExercisesInRoutineDao {
 
     @Query("SELECT exercises_in_routine.exerciseInRoutineId as exInRoutineId, exercises.exerciseId as exerciseId, exercises.name as exerciseName  FROM exercises_in_routine INNER JOIN exercises ON exercises_in_routine.fk_exerciseId = exercises.exerciseId WHERE exercises_in_routine.fk_routineId = :routineId ")
     LiveData<List<ExerciseInRoutineExercise>> getExerciseInRoutineAndExerciseByRoutineId(int routineId);
+
+//    @Query("SELECT ex.exerciseInRoutineId,ex.fk_routineId,lwp.latest_workout_params FROM exercises_in_routine AS ex INNER JOIN (SELECT fk_exerciseInRoutineId, Max(workoutParamsId) as latest_workout_params " +
+//            "from (SELECT * from workout_params WHERE workout_date is null) as wp " +
+//            "group by wp.fk_exerciseInRoutineId Having Max(workoutParamsId) ) as lwp on ex.exerciseInRoutineId = lwp.fk_exerciseInRoutineId " +
+//            "Where ex.fk_routineId = :routineId")
+//    List<ExerciseInRoutineWorkoutParams>getExercisesInRoutineWithLatestNullWorkoutParams(int routineId);
+
+//    @Query("SELECT ex.exerciseInRoutineId,ex.fk_routineId,lwp.latest_workout_params, s.* FROM exercises_in_routine AS ex " +
+//            "INNER JOIN (SELECT fk_exerciseInRoutineId, Max(workoutParamsId) as latest_workout_params " +
+//            "from (SELECT * from workout_params WHERE workout_date is null) as wp " +
+//            "group by wp.fk_exerciseInRoutineId Having Max(workoutParamsId) ) as lwp on ex.exerciseInRoutineId = lwp.fk_exerciseInRoutineId " +
+//            "INNER JOIN series s on lwp.latest_workout_params = s.fk_workoutParamsId " +
+//            "Where ex.fk_routineId = :routineId")
+//    Map<ExerciseInRoutineWorkoutParams,List<Series>>getExercisesWithSeries(int routineId);
+
+    @Query("SELECT ex.exerciseInRoutineId,ex.fk_routineId, ex.fk_exerciseId ,lwp.latest_workout_params, s.* FROM exercises_in_routine AS ex " +
+            "INNER JOIN (SELECT fk_exerciseInRoutineId, Max(workoutParamsId) as latest_workout_params " +
+            "from (SELECT * from workout_params WHERE workout_date is null) as wp " +
+            "group by wp.fk_exerciseInRoutineId Having Max(workoutParamsId) ) as lwp on ex.exerciseInRoutineId = lwp.fk_exerciseInRoutineId " +
+            "INNER JOIN series s on lwp.latest_workout_params = s.fk_workoutParamsId " +
+            "Where ex.fk_routineId = :routineId")
+    Map<ExerciseInRoutineWorkoutParams,List<Series>>getExercisesWithSeries(int routineId);
+
+
 
 
 }
