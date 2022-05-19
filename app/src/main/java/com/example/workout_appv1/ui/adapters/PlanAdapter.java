25bd40a41930c804b579b96a19d2af7c6a1312f7
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-05-19 20:19:02 +0200

    refactor PlanAdapter.java

--- app/src/main/java/com/example/workout_appv1/ui/adapters/PlanAdapter.java
+++ app/src/main/java/com/example/workout_appv1/ui/adapters/PlanAdapter.java
@@ -1,5 +1,6 @@
 package com.example.workout_appv1.ui.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,32 +26,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.MyViewHolder> {
-    List<Routine>routines=new ArrayList<>();
+    List<Routine> routines = new ArrayList<>();
     Context context;
-    String[]dayShortCuts;
-    FragmentPlanViewModel viewModel;
-    private final OnRoutineClickListener onRoutineClickListener;
+    private final IOnRoutineAction mIOnRoutineAction;
 
-    public PlanAdapter( Context context, String[] dayShortCuts,OnRoutineClickListener onRoutineClickListener) {
+    public PlanAdapter(Context context, IOnRoutineAction mIOnRoutineAction) {
         this.context = context;
-        this.dayShortCuts = dayShortCuts;
-        this.onRoutineClickListener=onRoutineClickListener;
-        this.viewModel = new ViewModelProvider((FragmentActivity)context).get(FragmentPlanViewModel.class);
+        this.mIOnRoutineAction = mIOnRoutineAction;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_fragment_plan,parent,false);
-        return new MyViewHolder(view,onRoutineClickListener);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_fragment_plan, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Routine routine=routines.get(position);
-        holder.tvNamePlanItem.setText(routine.getRoutineName());
-        holder.tvDayPlanItem.setText(getDayShortcut(routine.getDayOfWeek()));
-        holder.btnMorePlanItem.setOnClickListener(view -> showPopupMenu(holder));
+        holder.bind(routines.get(position), mIOnRoutineAction);
     }
 
     @Override
@@ -58,70 +52,57 @@ public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.MyViewHolder> 
         return routines.size();
     }
 
-    private String getDayShortcut(int day_of_week){
-        String shortcut="";
-        if(day_of_week<=6&&day_of_week>=0)
-            shortcut=dayShortCuts[day_of_week];
-        return shortcut;
-
-    }
-
-    private void showPopupMenu(MyViewHolder holder){
-        PopupMenu menu=new PopupMenu(context,holder.btnMorePlanItem);
-        menu.inflate(R.menu.item_popup_menu);
-        menu.setOnMenuItemClickListener(menuItem -> {
-            switch (menuItem.getItemId()){
-                case R.id.miEdit:
-                    editRoutine(routines.get(holder.getAdapterPosition()));
-                    return true;
-                case R.id.miDelete:
-                    delete(holder);
-                    return true;
-                default:
-                    return false;
-            }
-        });
-        menu.show();
-    }
-
-    public void setRoutines(List<Routine>routines){
+    public void setRoutines(List<Routine> routines) {
         this.routines = routines;
         notifyDataSetChanged();
     }
 
-    private void delete(MyViewHolder holder){
-        int position=holder.getAdapterPosition();
-        Routine r=routines.get(position);
-        viewModel.deleteRoutine(r);
-    }
-    private void editRoutine(Routine routine){
-        DialogAddEditRoutine dialog = DialogAddEditRoutine.newEditInstance(routine.getFk_planId(),routine.getRoutineId());
-        FragmentManager fragmentManager = ((AppCompatActivity)context).getSupportFragmentManager();
-        dialog.show(fragmentManager,"EditRoutine");
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tvDayPlanItem, tvNamePlanItem;
         ImageView btnMorePlanItem;
         ConstraintLayout clPlanItem;
-        OnRoutineClickListener onRoutineClickListener;
 
-        public MyViewHolder(@NonNull View itemView,OnRoutineClickListener onRoutineClickListener) {
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.tvDayPlanItem=itemView.findViewById(R.id.tvDayPlanItem);
-            this.tvNamePlanItem=itemView.findViewById(R.id.tvNamePlanItem);
-            this.btnMorePlanItem=itemView.findViewById(R.id.btnMorePlanItem);
-            this.clPlanItem=itemView.findViewById(R.id.clPlanItem);
-            this.onRoutineClickListener=onRoutineClickListener;
-            this.clPlanItem.setOnClickListener(this);
+            this.tvDayPlanItem = itemView.findViewById(R.id.tvDayPlanItem);
+            this.tvNamePlanItem = itemView.findViewById(R.id.tvNamePlanItem);
+            this.btnMorePlanItem = itemView.findViewById(R.id.btnMorePlanItem);
+            this.clPlanItem = itemView.findViewById(R.id.clPlanItem);
         }
 
-        @Override
-        public void onClick(View view) {
-            onRoutineClickListener.OnRoutineClick(routines.get(getAdapterPosition()));
+        @SuppressLint("NonConstantResourceId")
+        public void bind(Routine routine, IOnRoutineAction onRoutineAction) {
+            this.tvNamePlanItem.setText(routine.getRoutineName());
+            this.tvDayPlanItem.setText(onRoutineAction.getDayShortcut(routine));
+            this.btnMorePlanItem.setOnClickListener(view -> {
+                PopupMenu popupMenu = new PopupMenu(context, btnMorePlanItem);
+                popupMenu.inflate(R.menu.item_popup_menu);
+                popupMenu.setOnMenuItemClickListener(menuItem -> {
+                    switch (menuItem.getItemId()) {
+                        case R.id.miEdit:
+                            onRoutineAction.onRoutineEdit(routine);
+                            return true;
+                        case R.id.miDelete:
+                            onRoutineAction.onRoutineDelete(routine);
+                            return true;
+                        default:
+                            return false;
+                    }
+                });
+                popupMenu.show();
+            });
+            this.clPlanItem.setOnClickListener(view -> onRoutineAction.onRoutineClick(routine));
+
         }
     }
-    public interface OnRoutineClickListener{
-        void OnRoutineClick(Routine routine);
+
+    public interface IOnRoutineAction {
+        void onRoutineClick(Routine routine);
+
+        void onRoutineEdit(Routine routine);
+
+        void onRoutineDelete(Routine routine);
+
+        String getDayShortcut(Routine routine);
     }
 }
