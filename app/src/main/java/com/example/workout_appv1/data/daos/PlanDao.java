8ff7720800365f71598e0dff8774179597b8586a
Michal-Bienek <72919664+Michal-Bienek@users.noreply.github.com>
2022-06-13 23:22:31 +0200

    remove unused methods from PlanDao

--- app/src/main/java/com/example/workout_appv1/data/daos/PlanDao.java
+++ app/src/main/java/com/example/workout_appv1/data/daos/PlanDao.java
@@ -25,16 +25,10 @@ public interface PlanDao {
     @Update
     void updatePlan(Plan plan);
 
-    @Query("UPDATE plans SET isActive=:status WHERE planId==:id")
-    void updatePlanStatus(int id, boolean status);
-
     @Query("SELECT * FROM plans ORDER BY isActive DESC")
-    LiveData<List<Plan>>getSortedPlans();
+    LiveData<List<Plan>> getSortedPlans();
 
     @Query("SELECT * FROM plans where planId=:planId LIMIT 1")
-    LiveData<Plan>getPlanById(int planId);
+    LiveData<Plan> getPlanById(int planId);
 
-    @Transaction
-    @Query("Select * FROM plans WHERE planId=:planId")
-    LiveData<List<PlanWithRoutines>>getPlanWithRoutines(int planId);
 }
